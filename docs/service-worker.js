abe1d43e711d5bd82d96cf2538ea4a68a8383695
longplayer <bartaber@hotmail.com>
2019-07-14 17:46:04 +0200

    Update

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.18ddc762597020f11711ce3093a5791d.js"
+  "portfolio/precache-manifest.d0edd01387f454a870842ed21f6d6e4d.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "portfolio"});
