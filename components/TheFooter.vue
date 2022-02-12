78c9f677678b60b1e7f01535bf252a3a1a642729
longplayer <bartaber@hotmail.com>
2022-02-12 17:41:00 +0100

    Added social links to the footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,15 +1,48 @@
 <template>
   <footer>
     <div class="footer--inner">
-      <p>content</p>
+      <ul>
+        <li v-for="(link, id) in links" :key="id">
+          <a :href="link.url" :title="link.title" target="blank" rel="noopener noreferrer">
+            <base-icon :name="link.name" :icon-marge="0" />
+          </a>
+        </li>
+      </ul>
     </div>
     <span class="separator"></span>
   </footer>
 </template>
 
+<script>
+export default {
+  setup() {
+    const links = [
+      {
+        name: 'github',
+        title: 'Visit my Github profile',
+        url: 'https://github.com/longplayer',
+      },
+      {
+        name: 'gitlab',
+        title: 'Visit my Gitlab profile',
+        url: 'https://gitlab.com/longplayer',
+      },
+      {
+        name: 'twitter',
+        title: 'Visit my Twitter profile',
+        url: 'https://twitter.com/longplayer80/likes',
+      },
+    ]
+
+    return { links }
+  },
+}
+</script>
+
+
 <style lang="postcss" scoped>
 footer {
-  
+
   background-color: var(--blue-dark);
   @apply flex justify-center content-center;
 
@@ -31,9 +64,25 @@ footer {
   .footer--inner {
     flex: 0 0 90%;
     width: 90%;
-    height: 80%;
+    height: auto;
     z-index: 1;
     @apply relative m-auto text-gray-50;
+
+    ul {
+      display: block;
+      text-align: center;
+
+      li {
+        display: inline-block;
+        vertical-align: middle;
+        line-height: normal;
+
+        a {
+          display: block;
+          @apply p-4;
+        }
+      }
+    }
   }
 }
 </style>
